b08eeb0dbeae8980912cf89040168295f947bab1
Dashiloh <princeshiloh@yahoo.com>
2022-09-09 15:18:56 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Description
+*
+*Return: (0) if successful
+*betty style doc for function main goes there
+*/
+int main(void)
+	{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
