753ba5bfde66ac28ae360ab279865d9eb517e54f
Dashiloh <princeshiloh@yahoo.com>
2022-09-09 18:08:06 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Description
+*
+*Return: (0) if successful
+*betty style doc for function main goes there
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 9; i++)
+	{	
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
