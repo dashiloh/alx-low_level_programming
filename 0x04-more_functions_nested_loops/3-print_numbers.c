e9f082e7b0586d9371e9c4fdce13680da3c9a2ef
Dashiloh <princeshiloh@yahoo.com>
2022-09-15 19:17:56 +0000

    Update print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <ctype.h>
 /**
  * print_numbers - prints numbers 0 to 9
+ *
+ * Return: 0 upon sucess
  */
 void print_numbers(void)
-{
-	int i;
-
-	for (i = '0'; i <=  '9'; i++)
+{	int num;
 	{
-		_putchar(i);
+		for (num = '0'; num <= '9'; num++)
+			_putchar(num);
 	}
 	_putchar('\n');
 }
