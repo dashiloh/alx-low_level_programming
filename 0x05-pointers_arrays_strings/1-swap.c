777a5afc17868bd252f4912d8e7e8183b7b9fd85
Dashiloh <princeshiloh@yahoo.com>
2022-09-19 11:10:58 +0000

    Swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap values - swap values of two variables
+ * @a: values to swap
+ * @b: values to swap
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+	return (0);
+
+}
