8b9a9637a87ba7f7086ee585e18d2decc969a338
Dashiloh <princeshiloh@yahoo.com>
2022-10-03 15:53:53 +0000

    update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array
+ * @size: size of the array
+ * @c: char
+ * Return: array or null
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (arr == NULL)
+	{
+		fprintf(stderr, "error allocating memory\n");
+	}
+	else
+	{
+		i = 0;
+
+		while (i < size)
+		{
+			arr[i] = c;
+
+			i++;
+		}
+
+		arr[i] = '\0';
+
+	}
+
+	return (arr);
+
+	free(arr);
+}
